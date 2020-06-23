414aebe014d4231fc715ac1825d6c9505e55200f
Gilvan Henrique <gilvanh.15@gmail.com>
2020-06-23 11:36:34 -0300

    user list component and view

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import '../models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Gilvan',
+    email: 'gilvan@alunos.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375__340.png'
+  ),
+    '2': const User(
+    id: '2',
+    name: 'Marcus',
+    email: 'marcus@alunos.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png'
+  ),
+    '3': const User(
+    id: '3',
+    name: 'Ayanna',
+    email: 'ayanna@alunos.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825__340.png'
+  ),
+    '4': const User(
+    id: '4',
+    name: 'João',
+    email: 'joao@profs.com',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/01/21/11/avatar-1789663_960_720.png'
+  ),
+};
